ccdba93167f63e91f7cad544db93f3a074677ace
jowens520 <jonathan.owens@gmail.com>
2020-09-21 22:41:03 -0500

    Added comments to budgpy.py.

--- PyBank/budgpy.py
+++ PyBank/budgpy.py
@@ -3,16 +3,16 @@
 
 from pathlib import Path
 
-def summary_output(number_of_months_in_budget, net_total_amount_of_profits_and_losses, average_of_profit_and_losses, greatest_increase_of_profits_dictionary, greatest_decrease_of_losses_dictionary):
+def summary_output(number_of_months_in_budget, net_total_amount_of_profits_and_losses, 
+average_of_profit_and_losses, greatest_increase_of_profits_dictionary, greatest_decrease_of_losses_dictionary):
     # Capture file path to be written
     output_path = Path("./Output/summary.txt")
 
     # Open output path 
     filewriter = open(output_path, 'w+')
 
-    # Template to easily make changes to summary output in the future
+    # Formatting individual rows for summary output and concatinates each string
     summary_message = "" 
-
     summary_message += "Financial Analysis\n"
     summary_message += "----------------------------\n"
     summary_message += f"Total Months: {number_of_months_in_budget}\n"
@@ -20,6 +20,13 @@ def summary_output(number_of_months_in_budget, net_total_amount_of_profits_and_l
     summary_message += f"Average Change: ${average_of_profit_and_losses}\n"
     summary_message += f"Greatest Increase in Profits: {greatest_increase_of_profits_dictionary['Date']} (${greatest_increase_of_profits_dictionary['PnL']})\n"
     summary_message += f"Greatest Decrease in Profits: {greatest_decrease_of_losses_dictionary['Date']} (${greatest_decrease_of_losses_dictionary['PnL']})\n"
+    
+    # Outputs summary message to screen
     print(summary_message)
+
+    # Writes summary message to output path
     filewriter.write(summary_message)
+
+    # Closes file
     filewriter.close()
+
