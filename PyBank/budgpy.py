c63289da2a6e8405d3635d5dfc820946feda984e
jowens520 <jonathan.owens@gmail.com>
2020-09-21 23:01:51 -0500

    Updated comments for summary_output function. Begin creating defined function for greatest increase or decrease in profits.

--- PyBank/budgpy.py
+++ PyBank/budgpy.py
@@ -3,6 +3,9 @@
 
 from pathlib import Path
 
+
+# Accepts metric variables and prints a standard output to the screen along with a summary.txt file to a Resources directory.
+# The dictionaries must contain 'Date" and 'PnL" indecies.
 def summary_output(number_of_months_in_budget, net_total_amount_of_profits_and_losses, 
 average_of_profit_and_losses, greatest_increase_of_profits_dictionary, greatest_decrease_of_losses_dictionary):
     # Capture file path to be written
@@ -30,3 +33,32 @@ average_of_profit_and_losses, greatest_increase_of_profits_dictionary, greatest_
     # Closes file
     filewriter.close()
 
+
+# Determine the greatest increase or decrease in profits (date and amount) over the entire period.
+def greatest_increase_or_decrease
+# Value to assign initial greatest increase or decrease in profits or losses
+initial_assignment_of_profits = 0
+
+# Loop through list of profits and losses
+for a_change_in_profits_and_losses_item in change_in_profits_and_losses_list:
+    
+    # Assignment of initial value to greatest increase from list
+    if initial_assignment_of_profits == 0:
+
+        # Assignment of dictionary values to variables
+        date_value = a_change_in_profits_and_losses_item["Date"]
+        pnl_value = a_change_in_profits_and_losses_item["PnL"]
+
+        # Assignment of values as the greatest increase in profits dictionary
+        greatest_increase_of_profits_dictionary["Date"] = date_value
+        greatest_increase_of_profits_dictionary["PnL"] = pnl_value
+
+        # Initial assignment of greatest increase in profits is complete and no longer needs to be performed
+        initial_assignment_of_profits = 1
+
+    # Compare one profit or loss value to next value
+    if greatest_increase_of_profits_dictionary["PnL"] < a_change_in_profits_and_losses_item["PnL"]:
+        
+        # Assign profits and losses current item value and date to greatest increase of profits dictionary
+        greatest_increase_of_profits_dictionary["Date"] = a_change_in_profits_and_losses_item["Date"]
+        greatest_increase_of_profits_dictionary["PnL"] = a_change_in_profits_and_losses_item["PnL"]
