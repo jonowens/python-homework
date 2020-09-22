69d7db6e1a551012f0b512c4f1d105a9c29456ff
jowens520 <jonathan.owens@gmail.com>
2020-09-21 22:36:35 -0500

    Added budgpy.py to import defined functions for reusability and import budgpy as bgy within main.ipynb.

---
+++ PyBank/budgpy.py
@@ -0,0 +1,25 @@
+# The following defined functions are used to assist in the output and calculations for budget metric compilations.
+# Please use Python 3.8.5 and import the following library shown below.
+
+from pathlib import Path
+
+def summary_output(number_of_months_in_budget, net_total_amount_of_profits_and_losses, average_of_profit_and_losses, greatest_increase_of_profits_dictionary, greatest_decrease_of_losses_dictionary):
+    # Capture file path to be written
+    output_path = Path("./Output/summary.txt")
+
+    # Open output path 
+    filewriter = open(output_path, 'w+')
+
+    # Template to easily make changes to summary output in the future
+    summary_message = "" 
+
+    summary_message += "Financial Analysis\n"
+    summary_message += "----------------------------\n"
+    summary_message += f"Total Months: {number_of_months_in_budget}\n"
+    summary_message += f"Total: ${net_total_amount_of_profits_and_losses}\n"
+    summary_message += f"Average Change: ${average_of_profit_and_losses}\n"
+    summary_message += f"Greatest Increase in Profits: {greatest_increase_of_profits_dictionary['Date']} (${greatest_increase_of_profits_dictionary['PnL']})\n"
+    summary_message += f"Greatest Decrease in Profits: {greatest_decrease_of_losses_dictionary['Date']} (${greatest_decrease_of_losses_dictionary['PnL']})\n"
+    print(summary_message)
+    filewriter.write(summary_message)
+    filewriter.close()
